56ac766d459601b4d24cd448f813a268ab39f66f
emmanuelcuxe-2025091 <ecuxe-2025091@kinal.edu.gt>
2026-09-04 23:20:35 -0600

    feat: importa al archivo registro-producto.ts

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegistroProducto } from './components/registro-producto/registro-producto';
 
 @Component({
-  imports: [RouterOutlet],
   selector: 'app-root',
-  styleUrl: './app.css',
+  standalone: true,
+  imports: [RouterOutlet, RegistroProducto],
   templateUrl: './app.html',
+  styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Frontend');
+  title = 'Frontend';
 }
